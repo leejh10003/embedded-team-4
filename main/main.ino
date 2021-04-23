17873d00f4d3dd6e0e86c3d0b851807834d841d0
namo-1205 <82198668+namo-1205@users.noreply.github.com>
2021-04-23 13:51:25 +0900

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,27 @@
-#include <Arduino.h>
-#include "ArduinoSTL.h"
+#include <ArduinoJson.h>
+
 using namespace std;
 void setup() {
   Serial.begin(9600);
+  while (!Serial) continue;
+
+  StaticJsonDocument<200> doc;
+
+  char json[] = "{\"food\":[],\"shelf_life\":[]}";
+
+  auto error = deserializeJson(doc, json);
+  if (error) {
+    Serial.print(F("deserializeJson() failed with cod "));
+    Serial.println(error.c_str());
+    return;
+  }
+  const char* food = doc["food"];
+  int latitude = doc["shelf_life"];
+
+  Serial.println(food);
+  Serial.println(latitude, 6);
 }
+
 void loop() {
   return;
 }
