e86e085190dd5983909305b6b9f27c4292b1c50b
namo-1205 <82198668+namo-1205@users.noreply.github.com>
2021-04-26 13:22:48 +0900

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,57 +1,105 @@
 #include <WiFiEsp.h>
-#include <WiFiEspClient.h>
-#include <WiFiEspServer.h>
-#include <WiFiEspUdp.h>
 #include <ArduinoJson.h>
+#include "WiFiEsp.h"
 
+// Emulate Serial1 on pins 6/7 if not present
 #ifndef HAVE_HWSERIAL1
-#include "SoftwareSerial.h"
-SoftwareSerial Serial1(6,7);
 #endif
+#include "SoftwareSerial.h"
 
-char ssid[] = "Embedded";
-char pass[] = "12341234";
-int keyIndex = 0;
-int status = WL_IDLE_STATUS;
+SoftwareSerial Serial1(2, 3); // RX, TX
 
-WiFiEspServer server(80);
+char ssid[] = "{WIFI SSID}";            // your network SSID (name)
+char pass[] = "{WIFI Password}";        // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
-void setup(){
-  Serial.begin(115200);
+char server[] = ""; // 서버 API가 구현되면 채워 넣어야 함.
+
+// Initialize the Ethernet client object
+WiFiEspClient client;
+
+void setup()
+{
+  // initialize serial for debugging
+  Serial.begin(9600);
+
+  // initialize serial for ESP module
   Serial1.begin(9600);
+
+  // initialize ESP module
   WiFi.init(&Serial1);
 
-  if(WiFi.status() == WL_NO_SHIELD){
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFi shield not present");
+
+    // don't continue
     while (true);
   }
 
-  while(status!=WL_CONNECTED){
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
+
+    // Connect to WPA/WPA2 network
     status = WiFi.begin(ssid, pass);
   }
-
+  
+  // you're connected now, so print out the data
   Serial.println("You're connected to the network");
+
   printWifiStatus();
+  
+  Serial.println();
+  Serial.println("Starting connection to server...");
 
-  server.begin();
+  // if you get a connection, report back via serial
+  if (client.connect(server, 80)) {
+    Serial.println("Connected to server");
+
+    // Make a HTTP request
+    client.println("GET /asciilogo.txt HTTP/1.1");
+    client.println("Host: arduino.cc");
+    client.println("Connection: close");
+    client.println();
+  }
 }
 
-void loop(){
-  WiFiEspClient client = server.available();
+void loop()
+{
+  // if there are incoming bytes available
+  // from the server, read them and print them
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  // if the server's disconnected, stop the client
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("Disconnecting from server...");
+    client.stop();
+
+    // do nothing forevermore
+    while (true);
+  }
 }
 
-void printWifiStatus(){
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
+  // print your WiFi shield's IP address
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
 
-  Serial.println();
-  Serial.print("To see this page in action, open a browser to http://");
-  Serial.println(ip);
-  Serial.println();
+  // print the received signal strength
+  long rssi = WiFi.RSSI();
+  Serial.print("Signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
