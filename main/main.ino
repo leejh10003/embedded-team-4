04a1cbbac766268418e9aee1c2440291cd49b156
Lee, JunHyuk <leejh10003@gmail.com>
2021-04-29 02:00:58 +0900

    Implement server API client

--- main/main.ino
+++ main/main.ino
@@ -12,7 +12,48 @@ char ssid[] = "Embedded4";            // your network SSID (name)
 char pass[] = "12341234";        // your network password
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
-char server[] = ""; // 서버 API가 구현되면 채워 넣어야 함.
+char server[] = "http://ec2-13-125-229-44.ap-northeast-2.compute.amazonaws.com"; // 서버 API가 구현되면 채워 넣어야 함.
+class ImageProcess{
+  private:
+  static WiFiEspClient client;
+  static StaticJsonDocument<200> getInfoFromServer(const char* endpoint/*TODO: Image를 위한 argument 추가*/){
+    client.connect(server, 80);
+    ImageProcess::client.connect(server, 80);
+    const char* d;
+    while (client.available()) {
+      char c = client.read();
+      if(c == '\r') break;
+      Serial.write(c);
+      d += c;   
+    }
+    if (!client.connected()) {
+      Serial.println();
+      Serial.println("Disconnecting from server...");
+      client.stop();
+    }
+    if(d != NULL){
+      StaticJsonDocument<200> doc;
+    
+      auto error = deserializeJson(doc, d);
+      if (error) {
+          Serial.print(F("deserializeJson() failed with code "));
+          Serial.println(error.c_str());
+      } else {
+        return doc;
+      }
+    }
+  }
+  public:
+  static StaticJsonDocument<200> decodeQrCode/*TODO: Need to determine arguments for decodeQrCode*/(){
+    const char endpoint[] = "http://ec2-13-125-229-44.ap-northeast-2.compute.amazonaws.com/qrcode";
+    return ImageProcess::getInfoFromServer(endpoint);
+  }
+  static StaticJsonDocument<200> foodInfoGet(/*TODO: Need to determine arguments for foodInfoGet*/){
+    const char endpoint[] = "http://ec2-13-125-229-44.ap-northeast-2.compute.amazonaws.com/food";
+    return ImageProcess::getInfoFromServer(endpoint);
+  }
+};
+static WiFiEspClient ImageProcess::client;
 
 struct {
   const char* lb;
@@ -24,7 +65,7 @@ struct {
 int index = 0;
 
 // Initialize the Ethernet client object
-WiFiEspClient client;
+
 
 void printWifiStatus()
 {
@@ -81,7 +122,7 @@ void setup()
   Serial.println("Starting connection to server...");
 
   // if you get a connection, report back via serial
-  if (client.connect(server, 80)) {
+  /*if (client.connect(server, 80)) {
     Serial.println("Connected to server");
 
     // Make a HTTP request
@@ -89,54 +130,26 @@ void setup()
     client.println("Host: arduino.cc");
     client.println("Connection: close");
     client.println();
-  }
+  }*/
 }
 
 void loop()
 {
-  // if there are incoming bytes available
-  // from the server, read them and print them
-  const char* d;
-  while (client.available()) {
-    char c = client.read();
-    if(c == '\r') break;
-    Serial.write(c);
-    d += c;   
-  }
+  StaticJsonDocument<200> foodInfo = ImageProcess::foodInfoGet(); //TODO: Image를 받아와야 함
+  StaticJsonDocument<200> qrCodeData = ImageProcess::decodeQrCode(); //TODO: Image를 받아와야 함
+  stock[index].lb = qrCodeData["data"];
+  stock[index].ki = foodInfo["name"];
+  //stock[index].registertime = doc["registered_at"]; //서버에서 반환하지 않습니다. client에서 결정해야 합니다.
+  stock[index].maxavailable = foodInfo["max_availability"]; //조만간 서버에서 결정된 값으로 반환할 예정입니다. 현재는 psuedo하게 값을 10으로 반환합니다.
 
-  // if the server's disconnected, stop the client
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("Disconnecting from server...");
-    client.stop();
-
-    // do nothing forevermore
-    while (true);
-  }
-  
-  if(d != NULL){
-  StaticJsonDocument<200> doc;
-
-  auto error = deserializeJson(doc, d);
-  if (error) {
-      Serial.print(F("deserializeJson() failed with code "));
-      Serial.println(error.c_str());
-      return;
-  }
-
-  stock[index].lb = doc["label"];
-  stock[index].ki = doc["kind"];
-  stock[index].registertime = doc["registered_at"];
-  stock[index].maxavailable = doc["max_availability"];
-
-  index++;
+  /*index++;
   }
   else{
     index = 0;
-  }
+  }*/
 
   for(int i = 0; i < index; i++){
-    if(){//남은 시간이 얼마 안 남았을 때){
+    if(true){//TODO: 남은 시간이 얼마 안 남았을 때){
       // 재고정보 display 표시
     }
   }
